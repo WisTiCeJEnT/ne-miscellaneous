49cd95c42dd365206c5e2a912a9172ec14958bcb
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-01-30 21:24:50 +0700

    Mail sender bot

--- python-mail-sender/sender.py
+++ python-mail-sender/sender.py
@@ -1,17 +1,26 @@
 import smtplib
 import getpass
+import time
 
-def send_by_gmail(recipient,subject,msg):
+def send_by_gmail(recipient,subject,msg,sender_email,sender_password):
     try:
         server = smtplib.SMTP("smtp.gmail.com:587")
         server.ehlo()
         server.starttls()
-        sender_email = input("Sender email: ")
-        sender_password = getpass.getpass()
         server.login(sender_email,sender_password)
         server.sendmail(sender_email,recipient,f"Subject: {subject}\n\n{msg}")
         server.quit()
         print("Email have been sent")
     except:
         print("Error Connecting to Gmail server")
-send_by_gmail(input("Recipient email: "),input("Subject: "),input("Message: "))
+recipient = input("Recipient email: ")
+subject = input("Subject: ")
+msg = input("Message: ")
+sender_email = input("Sender email: ")
+sender_password = getpass.getpass()
+count = 0
+while(True):
+    count += 1
+    print(msg,count)
+    send_by_gmail(recipient,subject,msg,sender_email,sender_password)
+    time.sleep(5)
