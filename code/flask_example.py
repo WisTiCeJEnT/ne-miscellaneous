0219f7c4defc9f6de1a6ed8c4cecbe865ea77771
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-04-27 02:24:46 +0700

    json

--- code/flask_example.py
+++ code/flask_example.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 
 i = 0
 
@@ -12,7 +13,7 @@ def root():
 def iplusplus():
     global i
     i += 1
-    return str(i)
+    return json.dumps({'i': str(i)}, sort_keys=True, indent=4, separators=(',', ': '))
 
 if __name__ == "__main__":
     app.run(debug = False,host="0.0.0.0", port=5000)
