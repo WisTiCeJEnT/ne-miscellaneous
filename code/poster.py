b0cc08218ca7606554db4b6197196158b2a75083
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-05-01 17:49:55 +0700

    python poster

---
+++ code/poster.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def poster(url, payload):
+    headers = {'content-type': 'application/json'}
+
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    print(response.text)
+    print(response.json)
+
+def getter(url):
+    response = requests.get(url)
+    print(response.text)
+    print(response.json)
